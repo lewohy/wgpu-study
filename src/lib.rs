bb7d26795205f7a388256730613936ed13bfaf3e
lewohy <lwh8762@gmail.com>
2025-08-02 18:54:52 +0900

    Index Buffer

--- src/lib.rs
+++ src/lib.rs
@@ -47,18 +47,28 @@ impl Vertex {
 
 const VERTICES: &[Vertex] = &[
     Vertex {
-        position: [0.0, 0.5, 0.0],
-        color: [1.0, 0.0, 0.0],
-    },
+        position: [-0.0868241, 0.49240386, 0.0],
+        color: [0.5, 0.0, 0.5],
+    }, // A
     Vertex {
-        position: [-0.5, -0.5, 0.0],
-        color: [0.0, 1.0, 0.0],
-    },
+        position: [-0.49513406, 0.06958647, 0.0],
+        color: [0.5, 0.0, 0.5],
+    }, // B
     Vertex {
-        position: [0.5, -0.5, 0.0],
-        color: [0.0, 0.0, 1.0],
-    },
+        position: [-0.21918549, -0.44939706, 0.0],
+        color: [0.5, 0.0, 0.5],
+    }, // C
+    Vertex {
+        position: [0.35966998, -0.3473291, 0.0],
+        color: [0.5, 0.0, 0.5],
+    }, // D
+    Vertex {
+        position: [0.44147372, 0.2347359, 0.0],
+        color: [0.5, 0.0, 0.5],
+    }, // E
 ];
+
+const INDICES: &[u16] = &[0, 1, 4, 1, 2, 4, 2, 3, 4];
 
 /// 애플리케이션의 상태를 저장할 구조체This will store the state of our game
 pub struct State {
@@ -75,6 +85,8 @@ pub struct State {
     render_pipeline: wgpu::RenderPipeline,
     vertex_buffer: wgpu::Buffer,
     num_vertices: u32,
+    index_buffer: wgpu::Buffer,
+    num_indices: u32,
     window: Arc<Window>,
 }
 
@@ -237,6 +249,13 @@ impl State {
 
         let num_vertices = VERTICES.len() as u32;
 
+        let index_buffer = device.create_buffer_init(&wgpu::util::BufferInitDescriptor {
+            label: Some("Index Buffer"),
+            contents: bytemuck::cast_slice(INDICES),
+            usage: wgpu::BufferUsages::INDEX,
+        });
+        let num_indices = INDICES.len() as u32;
+
         Ok(Self {
             surface,
             device,
@@ -246,6 +265,8 @@ impl State {
             render_pipeline,
             vertex_buffer,
             num_vertices,
+            index_buffer,
+            num_indices,
             window,
         })
     }
@@ -325,12 +346,9 @@ impl State {
                 0,
                 self.vertex_buffer.slice(..),
             );
-            render_pass.draw(
-                // vertex buffer의 vertex갯수만큼 그림
-                0..self.num_vertices,
-                // 1번 그림
-                0..1,
-            );
+            render_pass.set_index_buffer(self.index_buffer.slice(..), wgpu::IndexFormat::Uint16);
+            // index buffer를 사용하여 그림
+            render_pass.draw_indexed(0..self.num_indices, 0, 0..1);
         }
 
         // command buffer를 종료하고 GPU에 제출
